36bb354e57bdf834d2f38946af08398c1fd0f7ce
Aswathi <aswathiap88@gmail.com>
2019-09-03 23:39:12 -0400

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeappAndroid()
+	{
+		System.out.println("NativeappAndroid");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
